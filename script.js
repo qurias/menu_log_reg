d64245d9d9645ac7b47e16fd5d0523bc7c61a6ea
Ivan M <vania.morskov@yandex.ru>
2021-05-17 14:51:51 +0300

    fix bugs and many more things

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-let logreg_login = document.querySelector(".header__logreg__login")
-let logreg_register = document.querySelector(".header__logreg__register")
+const logreg_login = document.querySelector('.header__login');
+const logreg_register = document.querySelector('.header__register');
 
-let login_block = document.querySelector(".block__logreg__login")
-let register_block = document.querySelector(".block__logreg__register")
+const login_block = document.querySelector('.block-login');
+const register_block = document.querySelector('.block-register');
 
-let login_info = document.querySelector(".block__logreg__login__info")
-let register_info  = document.querySelector(".block__logreg__register__info")
+const login_info = document.querySelector('.block-login__info');
+const register_info  = document.querySelector('.block-register__info');
 
 logreg_login.addEventListener('click', () => {
-  login_block.style.display = 'flex'
-  register_block.style.display = 'none'
-  register_info.style.display = 'none'
-  setTimeout(() => login_info.style.display = 'flex' , 800)
-})
+  login_block.style.display = 'flex';
+  register_block.style.display = 'none';
+  register_info.style.display = 'none';
+  setTimeout(() => login_info.style.display = 'flex' , 800);
+});
 
 logreg_register.addEventListener('click', () => {
-  register_block.style.display = 'flex'
-  login_block.style.display = 'none'
-  login_info.style.display = 'none'
-  setTimeout(() => register_info.style.display = 'flex' , 800)
-})
+  register_block.style.display = 'flex';
+  login_block.style.display = 'none';
+  login_info.style.display = 'none';
+  setTimeout(() => register_info.style.display = 'flex' , 800);
+});
